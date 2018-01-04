2337962d474c4221df37329dec7232222d7aaae0
Miguel Cubias <miguel.cubias@telusinternational.com>
2018-01-03 18:00:32 -0600

    Integrating Force dot com first tests

--- src/main/java/org/cubias/McBootApplication.java
+++ src/main/java/org/cubias/McBootApplication.java
@@ -6,19 +6,32 @@ import org.cubias.entities.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sforce.soap.enterprise.Connector;
+import com.sforce.soap.enterprise.EnterpriseConnection;
+import com.sforce.ws.ConnectionException;
+import com.sforce.ws.ConnectorConfig;
+
 @SpringBootApplication
 public class McBootApplication {
 
 	public static HashMap<Long, Student> hmapStudent;
+	static final String USERNAME = "qlik.datasource@telusinternational.com.fulltest";
+	static final String PASSWORD = "1XcLcenKm3oP56ATDz2iTv74JePdSaFHU8mpKVrpMJxW";
+	public static EnterpriseConnection connection;
 
 	public static void main(String[] args) {
-		hmapStudent = new HashMap<Long, Student>();
-		Student s1 = new Student("Miguel", "Math");
-		hmapStudent.put(s1.getId(), s1);
-		
-		SpringApplication.run(McBootApplication.class, args);
-		
-		Student s2 = new Student("Cubias", "History");
-		hmapStudent.put(s2.getId(), s2);
+		ConnectorConfig config = new ConnectorConfig();
+		config.setUsername(USERNAME);
+		config.setPassword(PASSWORD);
+		try {
+			connection = Connector.newConnection(config);
+			System.out.println("Auth EndPoint: " + config.getAuthEndpoint());
+			System.out.println("Service EndPoint: " + config.getServiceEndpoint());
+			System.out.println("Username: " + config.getUsername());
+			System.out.println("SessionId: " + config.getSessionId());
+			SpringApplication.run(McBootApplication.class, args);
+		} catch (ConnectionException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/org/cubias/controllers/HomeController.java
+++ src/main/java/org/cubias/controllers/HomeController.java
@@ -1,12 +1,17 @@
 package org.cubias.controllers;
 
+import java.util.ArrayList;
 import java.util.Map;
 
+import org.cubias.McBootApplication;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.sforce.soap.enterprise.QueryResult;
+import com.sforce.soap.enterprise.sobject.Contact;
 
 @Controller
 @RequestMapping(value = "/")
@@ -28,4 +33,30 @@ public class HomeController {
 		return modelAndView;
 	}
 
+	@RequestMapping(value = "contacts", method = RequestMethod.GET)
+	public ModelAndView contractsIndex() {
+		ArrayList<Contact> contact = new ArrayList<Contact>();
+		ModelAndView modelAndView = new ModelAndView("contract_list");
+		try {
+
+			// query for the 5 newest contacts
+			QueryResult queryResults = McBootApplication.connection
+					.query("SELECT Id, FirstName, LastName, Account.Name "
+							+ "FROM Contact WHERE AccountId != NULL ORDER BY CreatedDate DESC LIMIT 15");
+			if (queryResults.getSize() > 0) {
+				for (int i = 0; i < queryResults.getRecords().length; i++) {
+					// cast the SObject to a strongly-typed Contact
+					Contact c = (Contact) queryResults.getRecords()[i];
+					contact.add(c);
+					System.out.println("Id: " + c.getId() + " - Name: " + c.getFirstName() + " " + c.getLastName()
+							+ " - Account: " + c.getAccount().getName());
+				}
+				modelAndView.addObject("contactList", contact);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return modelAndView;
+	}
 }
